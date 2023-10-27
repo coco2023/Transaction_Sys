59906aef3e037d7480a85f3b3f3d63cd7bcd5e86
keyuhan <hankeyu2021@gmail.com>
2023-10-27 01:33:46 -0500

    add AJAX for success-checkout page

--- stripe-hosted-page-customer-checkout/public/script.js
+++ stripe-hosted-page-customer-checkout/public/script.js
@@ -1,37 +1,59 @@
 $(document).ready(function () {
-    // Replace with your Stripe API endpoint for retrieving payment events
-    const stripeEndpoint = 'https://api.stripe.com/v1/events/cs_test_a1sYAXCQ3Kyx4KeRq4VxrWZoiTSoaV1x9E9ZwKgMDSBBuDxuEN';
+  // Function to extract query parameters from the URL
+  function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+  }
 
-    // Make an AJAX request to fetch the payment event data from Stripe
-    $.ajax({
-        url: stripeEndpoint,
-        type: 'GET',
-        beforeSend: function (xhr) {
-            // Set the Stripe API key in the request headers
-            xhr.setRequestHeader('Authorization', 'Bearer YOUR_STRIPE_API_KEY');
-        },
-        success: function (data) {
-            // Function to display checkout session details
-            function displayCheckoutDetails(data) {
-                const checkoutDetails = document.getElementById('checkout-details');
-                checkoutDetails.innerHTML = `
-                    <p>ID: ${data.id}</p>
-                    <p>Object: ${data.object}</p>
-                    <p>Customer Email: ${data.customer_email}</p>
-                    <p>Custom ID: ${data.metadata.custom_id}</p>
-                    <p>Custom Order ID: ${data.metadata.custom_order_id}</p>
-                    <p>Mode: ${data.mode}</p>
-                    <p>Payment Intent: ${data.payment_intent}</p>
-                    <p>Payment Status: ${data.payment_status}</p>
-                    <p>Phone Number Collection Enabled: ${data.phone_number_collection.enabled}</p>
-                `;
-            }
+  // Get the payment_intent_id & stripe_api_key from the URL
+  const paymentIntentId = getQueryParam("payment_intent_id");
+  const stripeAPIKey = getQueryParam("stripe_api_key");
+  console.log(
+    "paymentIntentId: " + paymentIntentId,
+    "stripeAPIKey: " + stripeAPIKey
+  );
 
-            // Call the function to display checkout session details
-            displayCheckoutDetails(data.object);
-        },
-        error: function (xhr, textStatus, error) {
-            console.error('Error fetching payment event data:', error);
-        }
-    });
+  // Replace with your Stripe API endpoint for retrieving payment events
+  const stripeEndpoint = `https://api.stripe.com/v1/checkout/sessions/${paymentIntentId}`;
+  console.log("stripeEndpoint: " + stripeEndpoint);
+
+  // Make an AJAX request to fetch the payment event data from Stripe
+  $.ajax({
+    url: stripeEndpoint,
+    type: "GET",
+    dataType: "json",
+    beforeSend: function (xhr) {
+      // Set the Stripe API key in the request headers
+      xhr.setRequestHeader("Authorization", `Bearer ${stripeAPIKey}`);
+    },
+    success: function (data) {
+      console.log("data: " + JSON.stringify(data));
+
+      const checkoutDetails = document.getElementById("checkout-details");
+
+      // Function to display checkout session details
+      function displayCheckoutDetails(data) {
+        // Create HTML content to display the details
+        const detailsHTML = `
+                        <h2>Checkout Session Details</h2>
+                        <p>ID: ${data.id}</p>
+                        <p>Payment Intent ID: ${data.payment_intent}</p>
+                        <p>Object: ${data.object}</p>
+                        <p>Customer: ${data.customer_details.name}</p>
+                        <p>Customer Email: ${data.customer_email}</p>
+                        <p>Customer ID: ${data.metadata.custom_id}</p>
+                        <p>Order ID: ${data.metadata.custom_order_id}</p>
+                    `;
+
+        // Update the 'order-details' div with the details
+        checkoutDetails.innerHTML = detailsHTML;
+      }
+
+      // Call the function to display checkout session details
+      displayCheckoutDetails(data);
+    },
+    error: function (xhr, textStatus, error) {
+      console.error("Error fetching payment event data:", error);
+    },
+  });
 });
--- stripe-hosted-page-customer-checkout/src/main/java/com/stripe/sample/Server.java
+++ stripe-hosted-page-customer-checkout/src/main/java/com/stripe/sample/Server.java
@@ -1,31 +1,20 @@
 package com.stripe.sample;
 
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
-
-import spark.ModelAndView;
-import spark.Request;
-import spark.Response;
-
-import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.port;
 import static spark.Spark.staticFiles;
 
 import com.stripe.Stripe;
-import com.stripe.model.PaymentIntent;
 import com.stripe.model.checkout.Session;
 import com.stripe.param.checkout.SessionCreateParams;
 
-import spark.Route;
-import spark.Spark;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class Server {
 
   public static void main(String[] args) {
+    String YOUR_DOMAIN = "http://localhost:4242";
     port(4242);
 
     // Load environment variables from .env file
@@ -37,13 +26,13 @@ public class Server {
 
     post("/create-checkout-session", (request, response) -> {
 
-      String YOUR_DOMAIN = "http://localhost:4242";
       String orderId = request.queryParams("order_id");
+      String customerId = request.queryParams("customer_id");
+      String currency = "usd";
       String product_1 = "iphone13";
       String image_1 = "https://ss7.vzw.com/is/image/VerizonWireless/apple-iphone-13-pink-09142021?fmt=webp";
       String product_2 = "iphone15";
       String image_2 = "https://ss7.vzw.com/is/image/VerizonWireless/iphone-14-purple-fall22-a";
-      String customerId = request.queryParams("customer_id");
       Long totalPrice = Long.valueOf(request.queryParams("total-price"));
       Long quantity = 1L;
       String customerEmail = "work@gmail.com";
@@ -63,7 +52,7 @@ public class Server {
                   .setQuantity(quantity) // the quantity of the line item being purchased
                   .setPriceData( // Data used to generate a new Price object inline
                       SessionCreateParams.LineItem.PriceData.builder()
-                          .setCurrency("usd")
+                          .setCurrency(currency)
                           .setUnitAmount(totalPrice) // Amount in cents representing how much to charge (e.g., $50.00)
                           .setProductData( // Data used to generate a new product object inline
                               SessionCreateParams.LineItem.PriceData.ProductData.builder()
